dc9354cf04a72d6347ab4fd99af8e5b8c5062cec
Juan <Juan Salazar>
2018-02-17 13:50:01 -0800

    a lot of changes

---
+++ js/accountRecovery.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("accRecoveryForm").reset();
+    alert("An email has been sent with a temporary password. You'll be redirected to the Log In page");
+}
